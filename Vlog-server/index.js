5ccb5ebd26817f4ca6e6c36ed01d38a31893b309
aslamkhanpatha142 <khanaslampathan142@gmail.com>
2025-10-01 00:04:53 +0530

    Update backend URL

--- Vlog-server/index.js
+++ Vlog-server/index.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(cors(
 {
-  origin: "*", // or your Vercel domain
+  origin: "https://blog-app-with-mern-stack-ten.vercel.app", // or your Vercel domain
   methods: "GET,POST,PUT,DELETE",
   credentials: true
 }
